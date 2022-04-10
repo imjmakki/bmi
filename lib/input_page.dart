664849ff863f847be155bcece883c47d58880eaa
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 16:37:46 +0300

    BMI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -46,9 +46,9 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key? key,
-  }) : super(key: key);
+  ReusableCard()
+
+  late Color colour;
 
   @override
   Widget build(BuildContext context) {
