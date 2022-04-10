75be4137c5091a55c458db32a8df8221aff84934
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 16:11:51 +0300

    BMI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,10 +13,10 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Container(
-        color: Color(0xFF1D1E33),
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10.0),
+          color: Color(0xFF1D1E33),
         ),
         height: 200,
         width: 170.0,
