f6be5eb018d986452021aa3709be2ee1f2510996
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 16:40:59 +0300

    BMI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,24 +18,34 @@ class _InputPageState extends State<InputPage> {
               child: Row(
             children: [
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
               ),
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
               ),
             ],
           )),
           Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
           ),
           Expanded(
               child: Row(
             children: [
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
               ),
               Expanded(
-                child: ReusableCard(),
+                child: ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
               ),
             ],
           )),
@@ -55,7 +65,7 @@ class ReusableCard extends StatelessWidget {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
