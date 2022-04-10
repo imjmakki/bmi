304e59281bc45c158522f498522b9f8898a99f35
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 16:21:41 +0300

    BMI

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -17,16 +17,56 @@ class _InputPageState extends State<InputPage> {
           Expanded(
               child: Row(
             children: [
-              Expanded(child: child),
-              Expanded(child: child),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
             ],
           )),
-          Expanded(child: child),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+          ),
           Expanded(
               child: Row(
             children: [
-              Expanded(child: child),
-              Expanded(child: child),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
             ],
           )),
         ],
