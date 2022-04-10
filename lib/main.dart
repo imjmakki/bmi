4e70bc8d0a87c345e93f4c47168c0bb621e67247
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 15:24:51 +0300

    BMI

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,9 @@ class BMICalculator extends StatelessWidget {
         primaryColor: const Color(0xFF0A0E21),
         scaffoldBackgroundColor: const Color(0xFF0A0E21),
         accentColor: Colors.purple,
-        textTheme: const TextTheme(),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.white),
+        ),
       ),
       home: InputPage(),
     );
