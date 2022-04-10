4684a45187647179e8baff512424c6e00b538335
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 15:29:32 +0300

    BMI

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         accentColor: Colors.purple,
